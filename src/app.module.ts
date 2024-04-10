a137d3eed2afe524e7d0e3dad794d3605a6e4b92
Joseph Kelley <josephkelley91@gmail.com>
2024-04-10 10:46:54 -0400

    Defines connection to local mongodb at main app module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TaskModule } from './modules/task.modules';
+
+const mongodbConnectionString = 'mongodb://localhost:27017/tasks';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(mongodbConnectionString),
+    TaskModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
